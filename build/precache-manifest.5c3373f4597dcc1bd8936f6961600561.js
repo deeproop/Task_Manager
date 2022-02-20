a060498ffc1fda77556ee1ff79c0f3f26780cf6e
Deep Roop <deepsinghroop@gmail.com>
2022-02-20 23:29:41 +0530

    first commit

---
+++ build/precache-manifest.5c3373f4597dcc1bd8936f6961600561.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e31eba83fdce6cf8fca36ed61c8b52e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19290bf6c3dd5caf03bd",
+    "url": "/static/js/2.7322f148.chunk.js"
+  },
+  {
+    "revision": "5750920ae906397f70df",
+    "url": "/static/js/main.0236f2a2.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
